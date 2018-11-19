b2402582152b3693614ee5ab3d252c0cf824bcfb
R K <blah@eample.com>
2018-11-19 14:27:38 +0100

    switched to bootswatch-slate

--- src/components/AppItem.js
+++ src/components/AppItem.js
@@ -6,8 +6,8 @@ export default ({
   return (
     <div className="col-lg-4 mt-2">
       <div className="card">
-        <div className="card-body">
-          <h5 className="card-title"> {title}</h5>
+        <div className="card-body bg-">
+          <h5 className="card-title "> {title}</h5>
           <p className="card-text">{description_long}</p>
         </div>
         <ul className="list-group list-group-flush">
