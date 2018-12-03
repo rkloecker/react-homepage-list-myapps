9ede4d543978b7feaf74bfb6c80b401bcf88404b
R K <blah@eample.com>
2018-12-03 16:54:10 +0100

    added ...loading

--- src/components/AppList.js
+++ src/components/AppList.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import axios from "axios";
 import AppItem from "./AppItem";
+import { Promise } from "rsvp";
 
 export default class AppList extends Component {
   constructor() {
     super();
     this.state = {
       url: "https://quiet-beyond-62095.herokuapp.com/",
-      Apps: []
+      Apps: [],
+      loading: true
     };
   }
 
@@ -16,11 +18,21 @@ export default class AppList extends Component {
     console.log(process.env);
   }
 
+  // just for testing purposes, simulating response delay
+  wait(data) {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        resolve(data);
+      }, 4000);
+    });
+  }
+
   getApps() {
     axios
       .get(`${this.state.url}api/apps`)
+      // .then(this.wait)
       .then(response => {
-        this.setState({ Apps: response.data }, () => {
+        this.setState({ Apps: response.data, loading: false }, () => {
           console.log(this.state);
         });
       })
@@ -31,6 +43,8 @@ export default class AppList extends Component {
     const AppItems = this.state.Apps.map((App, i) => {
       return <AppItem key={App._id} item={App} />;
     });
-    return <div className="row">{AppItems}</div>;
+    return (
+      <div className="row">{this.state.loading ? "...loading" : AppItems}</div>
+    );
   }
 }
